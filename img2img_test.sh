6fad66c7c3630b5ab54a2dfc7a6598039f904732
Zhao Shenyang <dev@zsy.im>
2022-09-22 17:57:11 +0800

    test scripts allow user input prompt

--- img2img_test.sh
+++ img2img_test.sh
@@ -1,1 +1,9 @@
-curl -X POST http://127.0.0.1:3000/img2img -H 'Content-Type: multipart/form-data' -F img="@input.jpg" -F data='{"prompt":"a black and white cat"}' --output output.jpg
+#!/bin/bash
+echo -n "enter prompt (e.g. two robots standing): "
+while read PROMPT && [ -z "$PROMPT" ]; do echo -n "enter prompt (e.g. two robots standing): "; done
+if [[ -n "$PROMPT" ]];
+then
+    curl -X POST http://127.0.0.1:3000/img2img -H 'Content-Type: multipart/form-data' -F img="@input.jpg" -F data="{\"prompt\":\"$PROMPT\"}" --output output.jpg
+else
+    echo "No input"
+fi
--- txt2img_test.sh
+++ txt2img_test.sh
@@ -1,1 +1,9 @@
-curl -X POST http://127.0.0.1:3000/txt2img -H 'Content-Type: application/json' -d '{"prompt":"a black and white cat"}' --output output.jpg
+#!/bin/bash
+echo -n "enter prompt (e.g. a bento box): "
+while read PROMPT && [ -z "$PROMPT" ]; do echo -n "enter prompt (e.g. a bento box): "; done
+if [[ -n "$PROMPT" ]];
+then
+    curl -X POST http://127.0.0.1:3000/txt2img -H 'Content-Type: application/json' -d "{\"prompt\":\"$PROMPT\"}" --output output.jpg
+else
+    echo "No input"
+fi
