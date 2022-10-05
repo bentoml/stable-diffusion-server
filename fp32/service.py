cdbdd98cb87e8c78c0480463e5cb30f6efd9f2fb
Bozhao <yubz86@gmail.com>
2022-10-04 22:48:34 -0700

    add inapint endpoint

--- fp32/service.py
+++ fp32/service.py
@@ -4,6 +4,7 @@ import torch
 from torch import autocast
 from diffusers import StableDiffusionPipeline
 from diffusers import StableDiffusionImg2ImgPipeline
+from diffusers import StableDiffusionInpaintPipeline
 
 from pydantic import BaseModel
 import bentoml
@@ -31,6 +32,16 @@ class StableDiffusionRunnable(bentoml.Runnable):
             feature_extractor=txt2img_pipe.feature_extractor,
         ).to(self.device)
 
+        self.inpaint_pipe = StableDiffusionInpaintPipeline(
+            vae=self.txt2img_pipe.vae,
+            text_encoder=self.txt2img_pipe.text_encoder,
+            tokenizer=self.txt2img_pipe.tokenizer,
+            unet=self.txt2img_pipe.unet,
+            scheduler=self.txt2img_pipe.scheduler,
+            safety_checker=self.txt2img_pipe.safety_checker,
+            feature_extractor=self.txt2img_pipe.feature_extractor,
+        ).to(self.device)
+
     @bentoml.Runnable.method(batchable=False, batch_dim=0)
     def txt2img(self, data):
         prompt = data["prompt"]
@@ -40,6 +51,9 @@ class StableDiffusionRunnable(bentoml.Runnable):
         num_inference_steps = data.get('num_inference_steps', 50)
         generator = torch.Generator(self.device)
         generator.manual_seed(data.get('seed'))
+
+        if not data['safety_check']:
+            self.txt2img_pipe.safety_checker = lambda images, **kwargs: (images, False)
 
         with ExitStack() as stack:
             if self.device != "cpu":
@@ -75,6 +89,9 @@ class StableDiffusionRunnable(bentoml.Runnable):
         generator = torch.Generator(self.device)
         generator.manual_seed(data.get('seed'))
 
+        if not data['safety_check']:
+            self.img2img_pipe.safety_checker = lambda images, **kwargs: (images, False)
+
         with ExitStack() as stack:
             if self.device != "cpu":
                 _ = stack.enter_context(autocast(self.device))
@@ -85,6 +102,34 @@ class StableDiffusionRunnable(bentoml.Runnable):
                 strength=strength,
                 guidance_scale=guidance_scale,
                 num_inference_steps=num_inference_steps,
+                generator=generator,
+            ).images
+            image = images[0]
+            return image
+
+    @bentoml.Runnable.method(batchable=False, batch_dim=0)
+    def inpaint(self, image, mask, data):
+        prompt = data["prompt"]
+        strength = data.get('strength', 0.8)
+        guidance_scale = data.get('guidance_scale', 7.5)
+        num_inference_steps = data.get('num_inference_steps', 50)
+        generator = torch.Generator(self.device)
+        generator.manual_seed(data.get('seed'))
+
+        if not data['safety_check']:
+            self.inpaint_pipe.safety_checker = lambda images, **kwargs: (images, False)
+
+        with ExitStack() as stack:
+            if self.device != "cpu":
+                _ = stack.enter_context(autocast(self.device))
+
+            images = self.inpaint_pipe(
+                prompt=prompt,
+                init_image=image,
+                mask_image=mask,
+                strength=strength,
+                num_inference_steps=num_inference_steps,
+                guidance_scale=guidance_scale,
                 generator=generator,
             ).images
             image = images[0]
@@ -107,6 +152,7 @@ class Txt2ImgInput(BaseModel):
     height: int = 512
     width: int = 512
     num_inference_steps: int = 50
+    safety_check: bool = True
     seed: int = None
 
 @svc.api(input=JSON(pydantic_model=Txt2ImgInput), output=Image())
@@ -123,6 +169,7 @@ class Img2ImgInput(BaseModel):
     strength: float = 0.8
     guidance_scale: float = 7.5
     num_inference_steps: int = 50
+    safety_check: bool = True
     seed: int = None
 
 img2img_input_spec = Multipart(img=Image(), data=JSON(pydantic_model=Img2ImgInput))
@@ -131,6 +178,16 @@ def img2img(img, data, context):
     data = data.dict()
     data['seed'] = generate_seed_if_needed(data['seed'])
     image = stable_diffusion_runner.img2img.run(img, data)
+    for i in data:
+        context.response.headers.append(i, str(data[i]))
+    return image
+
+inpaint_input_spec = Multipart(img=Image(), mask=Image(), data=JSON(pydantic_model=Img2ImgInput))
+@svc.api(input=inpaint_input_spec, output=Image())
+def inpaint(img, mask, data, context):
+    data = data.dict()
+    data['seed'] = generate_seed_if_needed(data['seed'])
+    image = stable_diffusion_runner.inpaint.run(img, mask, data)
     for i in data:
         context.response.headers.append(i, str(data[i]))
     return image
